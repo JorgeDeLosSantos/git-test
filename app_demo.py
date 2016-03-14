1c03e8b0641c384e7d7d25017bb52d45eccc493a
Jorge De Los Santos <delossantosmfq@gmail.com>
2016-03-14 11:12:40 -0600

    New file

---
+++ app_demo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+# ---------------------------------------------------------
+# Author: Pedro Jorge De Los Santos
+# E-mail: delossantosmfq@gmail.com
+# License: MIT License
+# Test for Git
+# ---------------------------------------------------------
+import wx
+
+class MiAplicacion(wx.Frame):
+    def __init__(self,parent,title):
+        wx.Frame.__init__(self,parent,title=title,size=(250,200))
+        self.sz = wx.BoxSizer(wx.VERTICAL)
+        self.SetBackgroundColour("#ff00ff")
+        self.SetSizer(self.sz)
+        self.Centre(True)
+        self.Show()
+
+if __name__=='__main__':
+    version = "0.1.0"
+    app = wx.App()
+    frame = MiAplicacion(None, u"AppDemo %s"%(version))
+    app.MainLoop()
