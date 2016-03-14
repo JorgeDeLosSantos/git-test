0ba1829885276f1b746e9c6660e32e8fd397c450
Jorge De Los Santos <delossantosmfq@gmail.com>
2016-03-14 11:27:07 -0600

    0.1.1

--- app_demo.py
+++ app_demo.py
@@ -11,13 +11,13 @@ class MiAplicacion(wx.Frame):
     def __init__(self,parent,title):
         wx.Frame.__init__(self,parent,title=title,size=(250,200))
         self.sz = wx.BoxSizer(wx.VERTICAL)
-        self.SetBackgroundColour("#ff00ff")
+        self.SetBackgroundColour("#ff7700")
         self.SetSizer(self.sz)
         self.Centre(True)
         self.Show()
 
 if __name__=='__main__':
-    version = "0.1.0"
+    version = "0.1.1"
     app = wx.App()
     frame = MiAplicacion(None, u"AppDemo %s"%(version))
     app.MainLoop()
